fbaee17425916f6029c50d4335c74db2515351cd
Ericdid <ericdidom@gmail.com>
2023-09-27 17:37:07 +0200

    add.number

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+
 ?>
 
 
@@ -11,6 +13,14 @@
     <title>php-strong-password-generator</title>
 </head>
 <body>
+    <form action="passlength" method="get">
+        <h3>Scegli un numero da 5 a 15,questo sarà la lunghezza della password da generare</h3>
+        <input type="number" min="5" max="15" name="password_lenght" id="password_lenght"> 
+        <button>invia</button>
+
+
+
+    </form>
     
 </body>
 </html>

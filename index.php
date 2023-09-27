95b30560f7fd073532a20214f081aa7f2fd3d434
Ericdid <ericdidom@gmail.com>
2023-09-27 20:11:57 +0200

    add_function(not_working)

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ $has_pass = isset($_GET["password_lenght"]);
 
 function generate($has_pass){
     $chars ="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890è+òàù,.-<>|!£$%&/()=?^[]@#_:;§°ç*é";
-    $random_chars = '';
+    $random_chars = str_shuffle($chars);
+    return  $random_chars;
 
+    $pass_generated = generate();
 }
 
 
@@ -25,7 +27,7 @@ function generate($has_pass){
         <h3>Scegli un numero da 5 a 15,questo sarà la lunghezza della password da generare</h3>
         <input type="number" min="5" max="15" name="password_lenght" id="password_lenght" required> 
         <button>invia</button>
-        <h3 id="generated_password">password generata: </h3>
+        <h3 id="generated_password">password generata: <?php echo $pass_generated ?></h3>
 
 
 

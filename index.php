66f15e287608a4275e8f53969b87bf554d8733c3
Ericdid <ericdidom@gmail.com>
2023-10-02 00:02:01 +0200

    fix.pass_generator (working)

--- index.php
+++ index.php
@@ -2,18 +2,23 @@
 $has_pass = isset($_GET["password_length"]);
 $pass_length = (int) $_GET["password_length"] ?? " ";
 
-$chars ="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890è+òàù,.-<>|!£$%&/()=?^[]@#_:;§°ç*é";
-$min = 0;
-$max = strlen($chars) - 1;
 
-$pass_generated = "";
-for ($i = 0; $i < $pass_length; $i++ ) 
+
+if ($has_pass) {
+    
+    $chars ="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890^?=)(/&";
+    $min = 0;
+    $max = strlen($chars) - 1;
+    $pass_generated = "";
+    
+    for  ($i = 0; $i < $pass_length; $i++ ) { ;
     $random_index = rand($min, $max);
-    $random_char = $chars [$random_index ];
-    $pass_generated = $random_char;
-    var_dump($pass_generated)
-?>
+    $random_char = $chars [$random_index];
+    $pass_generated .= $random_char;
+    }
+}
 
+?>
 
 <!DOCTYPE html>
 
@@ -31,11 +36,6 @@ for ($i = 0; $i < $pass_length; $i++ )
         <input type="number" min="5" max="15" name="password_length" value="<?=$pass_length?>">
         <button>invia</button>
         <h3 id="generated_password">password generata: <?php echo $pass_generated ?></h3>
-
-
-
-
-
     </form>
 
 </body>
